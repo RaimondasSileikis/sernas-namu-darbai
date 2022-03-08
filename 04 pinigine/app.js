350dafee1be7c613c676dd061e7efbc88dd40fa7
Raimondas Sileikis <raimondas.sileikis@gmail.com>
2022-03-08 20:26:57 +0200

    04

--- 04 pinigine/app.js
+++ 04 pinigine/app.js
@@ -97,48 +97,87 @@ console.log('Popieriniu pinigu masyvas: ', pinigine2);
 // 9. Sukurti masyvą (piniginę su dviem skyreliais) iš dviejų elementų, kurio pirmas elementas būtų masyvas iš 8 uždavinio
 // su monetom, o antras elementas masyvas iš 8 uždavinio su popieriniais pinigais;
 
-const newPinigine = pinigine1.map(function (name, i) {
- 
-  return [name, pinigine2[i]]; 
-});
-
-console.log(newPinigine);
-  
+const newPinigine = [pinigine1, pinigine2];
+console.log('Pinigine su dviem skyreliais: ', newPinigine);
 
 
 // 10. Į 9 uždavinio masyvą, piniginę su dviem skyreliais, pridėti trečią skyrelį- masyvą su kortelėm: 
 // ['KIKA', 'Euro Vaistinė', 'Drogas', 'Ačiū', 'Lietuvos Geležinkeliai', 'Mano RIMI'];
-
-
-
+const kortele = ['KIKA', 'Euro Vaistinė', 'Drogas', 'Ačiū', 'Lietuvos Geležinkeliai', 'Mano RIMI'];
+newPinigine.push(kortele)
+ 
+console.log('Pinigine su trimis skyreliais :', newPinigine);
+ 
 // 11. Korteles skyrelyje sudėlioti (išrūšiuoti) pagal abėcėlę;
 
-
+newPinigine[kortele.sort()];
+console.log('Surusiuota: ', newPinigine);
 
 // 12.  Į kortelių skyrelį pridėti mokėjimo kortelių 'MasterCard', 'Visa' (su rand generuokite atsitiktines reikšmes
 // 'MasterCard' arba 'Visa' ir rašykite į masyvą) iš skirtingų bankų tiek, kad skyrelis (masyvo ilgis) pasidarytų lygus 20;
 
+while (kortele.length <= 20) {
+  kortele.push(!rand(0, 1) ? 'MasterCard':'Visa');
+}
 
-
+console.log('Pinigines korteleje prideta Mastercard ir Visa: ', newPinigine);
 
 // 13. Paskaičiuokite, kokio tipo kortelių ('MasterCard' arba 'Visa') yra daugiau;
-
+let sumM = 0;
+  let sumV = 0;
+for (let i = 0; i < kortele.length; i++) {
+  
+  if (kortele[i] === 'MasterCard') {
+    sumM += 1;
+  } 
+  if (kortele[i] === 'Visa') {
+    sumV += 1;
+  }
+}
+  console.log('MasterCard korteliu yra: ', sumM,'.', 'Visa korteliu yra: ', sumV,'.');
 
 
 // 14. Sukurkite masyve (piniginėje) ketvirtą elementą (skyrelį) į kurį įdėkite 10 loterijos bilietų, kurių numerius 
 // sugeneruokite atsitiktinai su rand funkcija nuo 1000000000 iki 9999999999;
+const skyrelis = [];
+for (let i = 0; i < 10; i++) {
+  
+  skyrelis.push(rand(1000000000, 9999999999));
+}
 
+console.log(skyrelis);
+newPinigine.push(skyrelis);
 
-
+console.log('Pinigineje pridetas ketvirtas elementas :',newPinigine);
 // 15. Loterijos bilietų masyvą išrūšiuoti nuo didžiausio numerio iki mažiausio;
 
-
+newPinigine[skyrelis.reverse()];
+console.log('Loterijos bilietu masyvas isrusiuotas nuo didziausio iki maziausio: ', newPinigine);
 
 // 16. Į piniginės popierinių pinigų skyrelį įdėti 500 pinigų mažom kupiūrom ( generuoti atsitiktinius skaičius nuo 3 iki 10
 // ir dėti kaip naujus elementus, kol įdėta suma bus lygi 500);
 
+/*const newMoney = [];
+let newM = 0;
+while (newM < 20) {
+  const i = rand(3, 10);
+  newMoney.push(i);
+  newM += i;
+}
+*/
+const newMoney = [];
+let newM = 0;
+do {
+  const i = rand(3, 10);
+  newMoney.push(i);
+  newM += i;
+} while (newM <= 500);
 
+console.log(newMoney);
+pinigine2.push(...newMoney);
 
+console.log(pinigine2);
+console.log(newPinigine);
 // 17. Patikrinti ar ką nors laimėjote. Bilieto numerius dalinkite iš 777 ir jeigu numeris išsidalins be liekanos - 
 // jūs laimėjote! Suskaičiuokite, kiek buvo laimingų bilietų.
 
