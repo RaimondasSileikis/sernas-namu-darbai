c37a658b8580047af6eecf981667e4d4d25a2914
Raimondas Sileikis <raimondas.sileikis@gmail.com>
2022-03-09 21:58:51 +0200

    05

--- 05 duomenu strukturos/app.js
+++ 05 duomenu strukturos/app.js
@@ -1,4 +1,4 @@
-console.log('Tarpinis žinių patikrinimas 04');
+console.log('Duomenu strukturos 05');
 
 function rand(min, max) {           //rand funkcija
     min = Math.ceil(min);
@@ -9,28 +9,54 @@ function rand(min, max) {           //rand funkcija
 // Duomenu strukturos
 const products = ['Dviratis', 'Triratis', 'Žvakidė', 'Stalas', 'Kėdė', 'Knygų lentyna', 'Paveikslas su rėmu', 'Veidrodis', 'Keturių vietų palapinė', 'Pripučiama valtis', 'Meškerė karosams gaudyti', 'Planšetė', 'Pastatoma lempa', 'Vazonas', 'Baterijų pakrovėjas', 'Pagalvių komplektas', 'Oro drėkintuvas', 'Oro sausintuvas', 'Kavinukas elektrinis', 'Kilimas 2 X 3 metrų', 'Kilimėlis kojoms valyti', 'Užuolaidos', 'Vėjo malūnas', 'Dažai sienoms', 'Dažai luboms', 'Medinė dėžė'];
 
-/* A. Sukurti 100 sąskaitų masyvą.
+// A. Sukurti 100 sąskaitų masyvą.
 
-Kiekviena sąskaita yra objektas ir turi po 5 savybes:
-number: pagal taisykle INV001, INV002, … INV099, INV100;
-products: masyvas random ilgio nuo 1 iki 10;
-products masyvo elementai objektai, turintys po 4 savybes:
-title: random iš products masyvo (vienoje sąskaitoje gali būti keli vienodi produktai. Vistiek jų kainos bus skirtingos); 
-price: random skaičius su kableliu nuo 1.00 iki 100.00; 
-amount: random skaičius nuo 1 iki 20;
-total:  price ir amount sandauga.
-total: visų products masyvo elementų total suma
-vat: 21% nuo total
-grandTotal: vat ir total suma
-*/
+//Kiekviena sąskaita yra objektas ir turi po 5 savybes:
+//number: pagal taisykle INV001, INV002, … INV099, INV100;
+//products: masyvas random ilgio nuo 1 iki 10;
+//products masyvo elementai objektai, turintys po 4 savybes:
+//1 title: random iš products masyvo (vienoje sąskaitoje gali būti keli vienodi produktai. Vistiek jų kainos bus skirtingos); 
+//2 price: random skaičius su kableliu nuo 1.00 iki 100.00; 
+//3 amount: random skaičius nuo 1 iki 20;
+//4 total:  price ir amount sandauga.
+//total: visų products masyvo elementų total suma
+//vat: 21% nuo total
+//grandTotal: vat ir total suma
 
-const saskaita = {};
-const number = [];
+const saskaitos = [];
+
 for (let i = 0; i < 100; i++) {
-  number.push('INV',[i]);
+  const productMasyvas = [];
+  let sumTotalSk = 0;
+
+for (let j = 0; j < rand(1, 10); j++) {
+  const priceSk = rand(1, 100).toFixed(2);
+  const amountSk = rand(1, 20);
+  const totalSk = priceSk * amountSk;
   
+  const productObject = {
+    title : products[Math.floor(Math.random() * products.length)],
+    price : priceSk,
+    amount : amountSk,
+    total : totalSk
+  };
+
+  
+  productMasyvas.push(productObject);
+
 }
-console.log(number);
+
+  const saskaita = {
+    number : ('INV' + [i]),
+    product : productMasyvas,
+    total: sumTotalSk               // NEPABAIGTA
+  };
+  
+  saskaitos.push(saskaita);
+}
+
+console.log(saskaitos);
+
 
 /* Sugeneruotame (ne generavimo metu!) masyve paskaičiuoti ir konsolėje atspausdinti visų sąskaitų grandTotal sumą, produktų sąrašą prie kiekvieno produkto pavadinimo pridedant koks to produkto kiekis yra visose sąskaitose bendrai ir už kokią bendrą sumą.
 
