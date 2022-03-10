df21dd06ce206d932f017235b722b49d7f9707af
Raimondas Sileikis <raimondas.sileikis@gmail.com>
2022-03-10 20:37:05 +0200

    02

--- 02/app.js
+++ 02/app.js
@@ -9,7 +9,7 @@ function rand(min, max) {           //rand funkcija
 // Suprogramuokite for ciklą, kuris imituotų 10 ratų automobilio važiavimą ir kiekviename 
 //cikle pateiktų (atspausdintų) kiek ratų dar liko automobiliui nuvažiuoti. Paskutinis skaičius 
 //turėtų būti 1. Visas rezultatas turėtų būti toks:  10 9 8 7 6 5 4 3 2 1 (skaičiai gali būti atspausdinti stulpeliu).
-
+console.log('1 uzduotis');
 
 for (let i = 0; i < 10; i++) {
    const likoRatu = 10 - i;
@@ -20,7 +20,7 @@ for (let i = 0; i < 10; i++) {
 // Kiekvieną ratą automobilis važiuoja skirtingu nuo 120 iki 220 km/h greičiu. Suprogramuokite 
 //for ciklą, kuris imituotų 10 ratų automobilio važiavimą, rand() funkcija generuokite atsitiktinį 
 //automobilio greitį o visiems ciklams pasibaigus pateikite bendrą visų 10 ratų vidutinį greitį.
-
+console.log('2 uzduotis');
 
 let ratuGreiciai = [];
 for (let i = 0; i < 10; i++) {
@@ -35,6 +35,7 @@ console.log('Automobilio Nr.55 10 nuvaziuotu ratu vidutinis greitis', vidurkis, 
 // litrai kuro. Kiekviename rate automobilis sunaudoja atsitiktinį kiekį kuro: nuo 3 iki 6 litrų. Suprogramuokite
 // for ciklą, kuris imituotų 10 ratų automobilio važiavimą, o ciklams pasibaigus pateikite išvadą ar automobiliui
 // užteko kuro įveikti visus 10 ratų. Kurui pasibaigus ciklą nutraukite anksčiau laiko. 
+console.log('3 uzduotis');
 
 let kuroSum = 0;
 let kk = 0;
@@ -44,7 +45,7 @@ for (let i = 0; i < 10; i++) {
     if (44 / kuroSum < 1) {
         break;
     }
-     kk = kk + 1;
+     kk ++;
 }
 if (kuroSum <= 44) {
     console.log('Kuro uzteko. Automobilis Nr.55 nuvaziavo 10 ratu, kuro liko', 44 - kuroSum,'litrai');
@@ -56,6 +57,8 @@ if (kuroSum <= 44) {
 // yra 5 posūkiai, kuriuose automobilio greitis yra atsitiktinis dydis nuo 20 iki 120 km/h. Imituokite tokią situaciją 
 //dviem for ciklais (vienas 10 ratų, kitas 5 posūkiai) ir ciklams pasibaigus atspausdinkite mažiausią automobilio greitį
 // kažkuriame iš posūkių.
+console.log('4 uzduotis');
+
 const minGreiciuMasyvas = [];
 for (let i = 0; i < 10; i++) {
     const greiciuMasyvas = [];
@@ -77,6 +80,7 @@ console.log('Automobilio Nr55 maziausias posukio greitis is visu nuvaziuotu ratu
 //vienas kilometras vienas ciklo prasisukimas. Cikle rand() funkciją atsitiktinai generuokite visų įvykių tikimybę. 
 //Jeigu viename cikle įvyksta visi nepalankūs įvykiai: iššoka kengūra, vairuotojas nespėja pasukti vairo ir nespėja paspausti
 // stabdžių while ciklą baikite. Ciklo pabaigoje atspausdinkite kiek kilometų sugebėjo nuvažiuoti automobilis be avarijos.
+console.log('5 uzduotis');
 
 console.log('Lenktynese dalyvaujancio Automobilio Nr55 ivykiu seka:');
 let i = 0;
